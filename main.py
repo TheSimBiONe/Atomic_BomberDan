3204ad87531fc191fa0aab64bb9d8dac8c993a3d
[TheSimBiOne] <[thesimbione@gmail.com]>
2022-03-05 23:42:37 +0300

    add bonuses, death for bombers and momental restart

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import os
 import sys
+from random import choice
 
 
 def load_image(name, colorkey=None):
@@ -57,8 +58,10 @@ class Brick(pygame.sprite.Sprite):
 
     def update(self):
         if self.destroyed:
-            if self.cur_frame == 7:
-                self.rect = pygame.Rect(0, 0, 0, 0)
+            if self.cur_frame >= 7:
+                create_bonus = choice([0, 0, 0, 0, 1])
+                if create_bonus == 1:
+                    Bonus(self.rect.x, self.rect.y)
                 self.kill()
             elif self.counter % 5 == 0:
                 self.image = self.sheet[self.cur_frame]
@@ -68,11 +71,6 @@ class Brick(pygame.sprite.Sprite):
 
 class GameField:
     def __init__(self):
-        global bomber
-        global dan
-        global wall_hit
-        global brick_hit
-
         self.width = 15
         self.height = 11
         self.board = [[0] * width for _ in range(height)]
@@ -82,20 +80,20 @@ class GameField:
             for y in range(11):
                 for x in range(15):
                     if room[y][x] == 'b':
-                        brick_hit.append(Brick(x, y))
+                        Brick(x, y)
                     elif room[y][x] == 'w':
-                        wall_hit.append(Wall(x, y))
+                        Wall(x, y)
                     elif room[y][x] == 'f':
-                        bomber = Bomberman(x, y, 'Bomberman.png', pygame.K_r, pygame.K_g, pygame.K_f,
+                        Bomberman(x, y, 'Bomberman.png', pygame.K_r, pygame.K_g, pygame.K_f,
                                            pygame.K_d, pygame.K_s)
                     elif room[y][x] == 's':
-                        dan = Bomberman(x, y, 'Dan.png', pygame.K_UP, pygame.K_RIGHT, pygame.K_DOWN,
+                        Bomberman(x, y, 'Dan.png', pygame.K_UP, pygame.K_RIGHT, pygame.K_DOWN,
                                         pygame.K_LEFT, pygame.K_SLASH)
 
 
 class Bomberman(pygame.sprite.Sprite):
     def __init__(self, x, y, picture, u, r, d, l, bomb):
-        super().__init__(all_sprites)
+        super().__init__(all_sprites, bombers)
         self.controls = (u, r, d, l, bomb)
 
         # size and hitbox
@@ -126,8 +124,8 @@ class Bomberman(pygame.sprite.Sprite):
         self.collied = False
 
         # parameters
-        self.bombs = 3
-        self.flame = 320
+        self.bombs = 1
+        self.flame = 80
 
     def cut_sheet(self, sheet, columns, rows):
         group = []
@@ -172,12 +170,8 @@ class Bomberman(pygame.sprite.Sprite):
             if self.bombs > 0:
                 x = (self.rect.center[0] // 80) * 80
                 y = ((self.rect.center[1] + 10) // 80) * 80
-                if (x, y) not in [i[1] for i in list(filter(lambda z: z != 0, bomb_hit))]:
-                    bomb_hit[bomb_hit.index(0)] = (Bomb(self,
-                                                        x, y,
-                                                        self.flame,
-                                                        90, bomb_hit.index(0)),
-                                                       (x, y))
+                if (x, y) not in [(i.rect.x, i.rect.y) for i in bombs.sprites()]:
+                    Bomb(self, x, y, self.flame, 90)
                     self.bombs -= 1
 
     def draw(self, surface):
@@ -203,7 +197,7 @@ class Bomberman(pygame.sprite.Sprite):
         self.image = self.group[self.cur_frame]
 
         self.collied = False
-        for wall in wall_hit:
+        for wall in walls.sprites():
             if self.hitbox.colliderect(wall.rect):
                 self.collied = True
                 self.rect.x -= self.dx
@@ -216,21 +210,26 @@ class Bomberman(pygame.sprite.Sprite):
                                    abs(self.hitbox.center[0] - wall.rect.center[0]) * self.speed
 
         if not self.collied:
-            for brick in brick_hit:
+            for brick in bricks.sprites():
                 if self.hitbox.colliderect(brick.rect):
                     self.rect.x -= self.dx
                     self.rect.y -= self.dy
 
         if not self.collied:
-            for bomb in bomb_hit:
+            for bomb in bombs.sprites():
                 if bomb != 0:
-                    if self.hitbox.colliderect(bomb[0].rect) and bomb[0].do_push:
+                    if self.hitbox.colliderect(bomb.rect) and bomb.do_push:
                         self.rect.x -= self.dx
                         self.rect.y -= self.dy
 
+        for bonus in bonuses.sprites():
+            if self.hitbox.colliderect(bonus.rect):
+                bonus.upgrade(self)
+                bonus.kill()
+
 
 class Bomb(pygame.sprite.Sprite):
-    def __init__(self, bomber_, x, y, flame, time, pos):
+    def __init__(self, bomber_, x, y, flame, time):
         super().__init__(all_sprites, bombs)
         self.do_push = False
 
@@ -250,13 +249,12 @@ class Bomb(pygame.sprite.Sprite):
         self.bomber = bomber_
         self.flame = flame
         self.time = time
-        self.pos = pos
         self.rect = pygame.Rect(x, y, 80, 80)
         self.rect.x = x
         self.rect.y = y
 
     def update(self):
-        if len(list(filter(lambda b: self.rect.colliderect(b.hitbox), bombers_hit))) == 0:
+        if len(list(filter(lambda b: self.rect.colliderect(b.hitbox), bombers.sprites()))) == 0:
             self.do_push = True
 
         if self.counter % 5 == 0:
@@ -271,7 +269,6 @@ class Bomb(pygame.sprite.Sprite):
 
         if self.time == 0:
             self.bomber.bombs += 1
-            bomb_hit[self.pos] = 0
             Flame(self.rect.x, self.rect.y, self.flame, 'up')
             Flame(self.rect.x, self.rect.y, self.flame, 'right')
             Flame(self.rect.x, self.rect.y, self.flame, 'down')
@@ -296,10 +293,10 @@ class Flame(pygame.sprite.Sprite):
 
         # animation
         self.sheets = {
-            'up': (self.cut_sheet(pygame.transform.scale(load_image('Flame_u.png'), (72, 525)), 1, 5), 0, -40),
-            'right': (self.cut_sheet(pygame.transform.scale(load_image('Flame_r.png'), (525, 72)), 5, 1), 40, 0),
-            'down': (self.cut_sheet(pygame.transform.scale(load_image('Flame_d.png'), (72, 525)), 1, 5), 0, 40),
-            'left': (self.cut_sheet(pygame.transform.scale(load_image('Flame_l.png'), (525, 72)), 5, 1), -40, 0),
+            'up': (self.cut_sheet(pygame.transform.scale(load_image('Flame_u.png'), (72, 525)), 1, 5), 0, -80),
+            'right': (self.cut_sheet(pygame.transform.scale(load_image('Flame_r.png'), (525, 72)), 5, 1), 80, 0),
+            'down': (self.cut_sheet(pygame.transform.scale(load_image('Flame_d.png'), (72, 525)), 1, 5), 0, 80),
+            'left': (self.cut_sheet(pygame.transform.scale(load_image('Flame_l.png'), (525, 72)), 5, 1), -80, 0),
         }
         self.group = self.sheets[direct][0]
         self.cur_frame = 0
@@ -314,6 +311,7 @@ class Flame(pygame.sprite.Sprite):
         self.rect.center = (x + 40, y + 40)
 
     def update(self):
+        global is_restart
         self.cur_frame = (self.cur_frame + 1) % len(self.group)
         self.image = self.group[self.cur_frame]
         self.rect.x += self.dx
@@ -322,20 +320,30 @@ class Flame(pygame.sprite.Sprite):
         if self.length <= abs(self.distance):
             self.kill()
 
-        for wall in wall_hit:
+        for bomber in bombers.sprites():
+            if self.rect.colliderect(bomber.hitbox):
+                bomber.kill()
+                is_restart = True
+
+        for wall in walls.sprites():
             if self.rect.colliderect(wall.rect):
                 self.kill()
 
-        for brick in brick_hit:
+        for brick in bricks.sprites():
             if self.rect.colliderect(brick.rect):
                 brick.destroy()
                 self.kill()
 
-        for bomb in bomb_hit:
+        for bomb in bombs.sprites():
             if bomb != 0:
-                if self.rect.colliderect(bomb[0].rect):
-                    bomb[0].time = 16
+                if self.rect.colliderect(bomb.rect):
+                    bomb.time = 16
                     self.kill()
+
+        for bonus in bonuses.sprites():
+            if self.rect.colliderect(bonus.rect):
+                bonus.kill()
+                self.kill()
 
         self.distance += self.dx + self.dy
 
@@ -351,6 +359,39 @@ class Flame(pygame.sprite.Sprite):
         return group
 
 
+class Bonus(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__(all_sprites, bonuses)
+        sheet = self.cut_sheet(pygame.transform.scale(load_image('Bonuses.png'), (225, 80)), 3, 1)
+        self.sheets = {
+            'bomb': sheet[0],
+            'flame': sheet[1],
+            'speed': sheet[2]
+        }
+        self.rect = pygame.Rect(x, y, 80, 80)
+        self.type = choice(['bomb', 'bomb', 'flame', 'flame', 'speed'])
+        self.image = self.sheets[self.type]
+
+    def cut_sheet(self, sheet, columns, rows):
+        group = []
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                group.append(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)))
+        return group
+
+    def upgrade(self, bomber_):
+        if self.type == 'bomb':
+            bomber_.bombs += 1
+        elif self.type == 'flame':
+            bomber_.flame += 80
+        elif self.type == 'speed':
+            bomber_.speed += 2
+
+
 fps = 60
 clock = pygame.time.Clock()
 size = width, height = 1200, 880
@@ -358,46 +399,54 @@ size = width, height = 1200, 880
 screen = pygame.display.set_mode(size)
 screen.fill('ForestGreen')
 
-bomb_hit = [0, 0, 0, 0, 0, 0]
-wall_hit = []
-brick_hit = []
-flames_jit = []
-
 all_sprites = pygame.sprite.Group()
 bombers = pygame.sprite.Group()
 walls = pygame.sprite.Group()
 bricks = pygame.sprite.Group()
 bombs = pygame.sprite.Group()
 flames = pygame.sprite.Group()
+bonuses = pygame.sprite.Group()
 game = GameField()
 
-bombers_hit = [bomber, dan]
+is_restart = False
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    if is_restart:
+        for bomber_ in bombers.sprites():
+            bomber_.kill()
+        for wall_ in walls.sprites():
+            wall_.kill()
+        for brick_ in bricks.sprites():
+            brick_.kill()
+        for bomb_ in bombs.sprites():
+            bomb_.kill()
+        for flame_ in flames.sprites():
+            flame_.kill()
+        for bonus_ in bonuses.sprites():
+            bonus_.kill()
+        game = GameField()
+        is_restart = False
 
     # формирование кадра
     screen.fill((51, 118, 0))
     flames.update()
-    bomber.update()
-    dan.update()
+    bombers.update()
     bombs.update()
     bricks.update()
+    bonuses.update()
 
     # изменение игрового мира
     walls.draw(screen)
     bricks.draw(screen)
+    bonuses.draw(screen)
     bombs.draw(screen)
-    bomber.draw(screen)
-    dan.draw(screen)
+    bombers.draw(screen)
     flames.draw(screen)
     pygame.display.flip()
 
     # временная задержка
     clock.tick(fps)
-
-
-
